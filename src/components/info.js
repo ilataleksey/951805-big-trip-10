5dc147e1c9cf4e485fc9b6a41ab62e6b68aa2a4e
ilataleksey <ilataleksey@gmail.com>
2020-01-04 02:16:29 +0300

    Формирует разметку информации о маршруте на основе карточек-моков

--- src/components/info.js
+++ src/components/info.js
@@ -1,7 +1,23 @@
-export const createInfoTemplate = () => (
-  `<div class="trip-info__main">
-    <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
+import {MONTHS} from '../const.js';
 
-    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
-  </div>`
-);
+export const createInfoTemplate = (cards) => {
+
+  const firstCity = cards[0].city;
+  const lastCity = cards[cards.length - 1].city;
+
+  const start = `${MONTHS[cards[0].dates.start.getMonth()].toUpperCase()} ${cards[0].dates.start.getDate()}`;
+  let end = start;
+  if (cards[0].dates.start.getMonth() === cards[cards.length - 1].dates.end.getMonth()) {
+    end = `${cards[cards.length - 1].dates.end.getDate()}`;
+  } else {
+    end = `${MONTHS[cards[cards.length - 1].dates.start.getMonth()].toUpperCase()} ${cards[cards.length - 1].dates.start.getDate()}`;
+  }
+
+  return (
+    `<div class="trip-info__main">
+      <h1 class="trip-info__title">${firstCity} &mdash; ... &mdash; ${lastCity}</h1>
+
+      <p class="trip-info__dates">${start}&nbsp;&mdash;&nbsp;${end}</p>
+    </div>`
+  );
+};
