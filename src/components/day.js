517bcc81950ec4fb9d43a5895c18587f38270eaa
ilataleksey <ilataleksey@gmail.com>
2020-01-04 02:13:23 +0300

    Формирует разметку блока дня в зависимости от даты в карточке точки маршрута

--- src/components/day.js
+++ src/components/day.js
@@ -1,8 +1,17 @@
-export const createDayTemplate = () => (
-  `<li class="trip-days__item  day">
-    <div class="day__info">
-      <span class="day__counter">1</span>
-      <time class="day__date" datetime="2019-03-18">MAR 18</time>
-    </div>
-  </li>`
-);
+import {MONTHS} from '../const.js';
+import {formatTimeHTMLShort} from '../utils.js';
+
+export const createDayTemplate = (dayCount, date) => {
+
+  const day = `${MONTHS[date.getMonth()].toUpperCase()} ${date.getDate()}`;
+  const dayHTML = formatTimeHTMLShort(date);
+
+  return (
+    `<li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">${dayCount}</span>
+        <time class="day__date" datetime="${dayHTML}">${day}</time>
+      </div>
+    </li>`
+  );
+};
