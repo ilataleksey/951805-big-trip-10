abfa9bd3c2f4fe52c29b150c46daa4c042ae3560
ilataleksey <ilataleksey@gmail.com>
2020-01-04 02:11:45 +0300

    Формирует разметку карточки на основе на основе данных в карточке-моке

--- src/components/card.js
+++ src/components/card.js
@@ -1,36 +1,84 @@
-export const createCardTemplate = () => (
-  `<li class="trip-events__item">
-    <div class="event">
-      <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
-      </div>
-      <h3 class="event__title">Taxi to airport</h3>
+import {formatTimeShort} from '../utils.js';
+import {formatTimeHTML} from '../utils.js';
+import {MILSEC_IN_DAY} from '../const.js';
+import {MILSEC_IN_HOUR} from '../const.js';
+import {MILSEC_IN_MIN} from '../const.js';
 
-      <div class="event__schedule">
-        <p class="event__time">
-          <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
-          &mdash;
-          <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
-        </p>
-        <p class="event__duration">1H 30M</p>
-      </div>
-
-      <p class="event__price">
-        &euro;&nbsp;<span class="event__price-value">20</span>
-      </p>
-
-      <h4 class="visually-hidden">Offers:</h4>
-      <ul class="event__selected-offers">
-        <li class="event__offer">
-          <span class="event__offer-title">Order Uber</span>
+const createOffersMarkup = (additionOffers) => {
+  return additionOffers
+    .filter((offer) => {
+      return offer.chosen;
+    })
+    .map((offer) => {
+      return (
+        `<li class="event__offer">
+          <span class="event__offer-title">${offer.type}</span>
           &plus;
-          &euro;&nbsp;<span class="event__offer-price">20</span>
-         </li>
-      </ul>
+          &euro;&nbsp;<span class="event__offer-price">${offer.price}</span>
+        </li>`
+      );
+    })
+    .join(`\n`);
+};
 
-      <button class="event__rollup-btn" type="button">
-        <span class="visually-hidden">Open event</span>
-      </button>
-    </div>
-  </li>`
-);
+export const createCardTemplate = (card) => {
+  const {type, city, dates, price, offers} = card;
+
+  const startTimeShort = formatTimeShort(dates.start);
+  const endTimeShort = formatTimeShort(dates.end);
+  const startTimeHTML = formatTimeHTML(dates.start);
+  const endTimeHTML = formatTimeHTML(dates.end);
+
+  const durationDays = Math.floor(dates.duration / MILSEC_IN_DAY);
+  const durationHours = Math.floor((dates.duration % MILSEC_IN_DAY) / MILSEC_IN_HOUR);
+  const durationMinutes = Math.floor(((dates.duration % MILSEC_IN_DAY) % MILSEC_IN_HOUR) / MILSEC_IN_MIN);
+
+  let durationD = ``;
+  if (durationDays) {
+    durationD = durationDays > 10 ? `${durationDays}D` : `0${durationDays}D`;
+  }
+
+  let durationH = ``;
+  if (durationHours) {
+    durationH = durationHours > 10 ? `${durationHours}H` : `0${durationHours}H`;
+  }
+
+  let durationM = ``;
+  if (durationMinutes) {
+    durationM = durationMinutes > 10 ? `${durationMinutes}M` : `0${durationMinutes}M`;
+  }
+
+  const addOffers = createOffersMarkup(Array.from(offers));
+  return (
+    `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.action}.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">${type.description} ${city}</h3>
+
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="${startTimeHTML}">${startTimeShort}</time>
+            &mdash;
+            <time class="event__end-time" datetime="${endTimeHTML}">${endTimeShort}</time>
+          </p>
+          <p class="event__duration">${durationD} ${durationH} ${durationM}</p>
+        </div>
+
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
+        </p>
+
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          ${addOffers}
+        </ul>
+
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>`
+  );
+};
