cb666882424855eb3e2bd0bf820dcc2f685dbdb1
ilataleksey <ilataleksey@gmail.com>
2020-01-27 02:38:25 +0300

    Дорабатывает модель с учетом фильтра

--- src/models/points.js
+++ src/models/points.js
@@ -1,14 +1,31 @@
+import {getPointsByFilter} from '../utils/filter.js';
+import {FILTER_TYPE} from '../const.js';
+
 export default class Points {
   constructor() {
     this._points = [];
+
+    this._activeFilterType = FILTER_TYPE.EVERYTHING;
+
+    this._dataChangeHandlers = [];
+    this._filterChangeHandlers = [];
+  }
+
+  getPointsAll() {
+    return this._points;
   }
 
   getPoints() {
-    return this._points;
+    return this.getPointsByFilter(this._points, this._activeFilterType);
   }
 
   setPoints(points) {
     this._points = Array.from(points);
+  }
+
+  setFilter(filterType) {
+    this._activeFilterType = filterType;
+    this._filterChangeHandlers.forEach((handler) => handler());
   }
 
   updatePoint(id, point) {
@@ -21,5 +38,9 @@ export default class Points {
     this._points = [].concat(this._points.slice(0, pointIndex), point, this._points.slice(pointIndex + 1));
 
     return true;
+  }
+
+  setFilterChangeHandler(handler) {
+    this._filterChangeHandlers.push(handler);
   }
 }
