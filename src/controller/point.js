c8ddb326048ff7efc8f6df1e9996c2169e843a09
ilataleksey <ilataleksey@gmail.com>
2020-01-27 21:17:03 +0300

    Реализует логику добавления/удаления точек маршрута

--- src/controller/point.js
+++ src/controller/point.js
@@ -1,10 +1,28 @@
-import {render, replace, RenderPosition} from '../utils/render.js';
+import {render, replace, remove, RenderPosition} from '../utils/render.js';
 import PointComponent from '../components/card.js';
 import PointEditComponent from '../components/edit-card.js';
 
-const Mode = {
+export const Mode = {
+  ADDING: `adding`,
   DEFAULT: `default`,
   EDIT: `edit`,
+};
+
+export const EmptyPoint = {
+  type: ``,
+  destination: {
+    city: ``,
+    description: ``,
+  },
+  dates: {
+    start: null,
+    end: null,
+    duration: null,
+  },
+  price: null,
+  isFavorite: false,
+  offers: null,
+  photos: null,
 };
 
 export default class PointController {
@@ -12,6 +30,7 @@ export default class PointController {
     this._container = container;
     this._onDataChange = onDataChange;
     this._onViewChange = onViewChange;
+
     this._mode = Mode.DEFAULT;
 
     this._pointComponent = null;
@@ -21,9 +40,10 @@ export default class PointController {
   }
 
   // функция для генерации карточки точки маршрута внутри дня
-  render(point, i) {
+  render(point, i, mode) {
     const oldPointComponent = this._pointComponent;
     const oldPointEditComponent = this._pointEditComponent;
+    this._mode = mode;
 
     this._pointComponent = new PointComponent(point);
     this._pointEditComponent = new PointEditComponent(point, i);
@@ -41,15 +61,42 @@ export default class PointController {
 
     this._pointEditComponent.setSubmitHandler((evt) => {
       evt.preventDefault();
-      this._replaceEditToPoint();
+      const data = this._pointEditComponent.getData();
+      this._onDataChange(this, point, data);
     });
+    this._pointEditComponent.setDeleteButtonClickHandler(() => this._onDataChange(this, point, null));
 
-    if (oldPointEditComponent && oldPointComponent) {
-      replace(this._pointComponent, oldPointComponent);
-      replace(this._pointEditComponent, oldPointEditComponent);
-    } else {
-      render(this._container, this._pointComponent, RenderPosition.BEFOREEND);
+    switch (mode) {
+      case Mode.DEFAULT:
+        if (oldPointEditComponent && oldPointComponent) {
+          replace(this._pointComponent, oldPointComponent);
+          replace(this._pointEditComponent, oldPointEditComponent);
+          this._replaceEditToPoint();
+        } else {
+          render(this._container, this._pointComponent, RenderPosition.BEFOREEND);
+        }
+        break;
+      case Mode.ADDING:
+        if (oldPointEditComponent && oldPointComponent) {
+          remove(oldPointComponent);
+          remove(oldPointEditComponent);
+        }
+        document.addEventListener(`keydown`, this._onEscKeyDown);
+        render(this._container, this._pointEditComponent, RenderPosition.AFTERBEGINING);
+        break;
     }
+  }
+
+  setDefaultView() {
+    if (this._mode !== Mode.DEFAULT) {
+      this._replaceEditToPoint();
+    }
+  }
+
+  destroy() {
+    remove(this._pointEditComponent);
+    remove(this._pointComponent);
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
   }
 
   _onEscKeyDown(evt) {
@@ -57,6 +104,9 @@ export default class PointController {
 
     if (isEscKey) {
       evt.preventDefault();
+      if (this._mode === Mode.ADDING) {
+        this._onDataChange(this, EmptyPoint, null);
+      }
       this._replaceEditToPoint();
       document.removeEventListener(`keydown`, this._onEscKeyDown);
     }
@@ -70,14 +120,10 @@ export default class PointController {
   }
 
   _replaceEditToPoint() {
-    this._pointEditComponent.rerender();
+    document.removeEventListener(`keydown`, this._onEscKeyDown);
+    this._pointEditComponent.reset();
+
     replace(this._pointComponent, this._pointEditComponent);
     this._mode = Mode.DEFAULT;
-  }
-
-  setDefaultView() {
-    if (this._mode !== Mode.DEFAULT) {
-      this._replaceEditToPoint();
-    }
   }
 }
--- src/models/points.js
+++ src/models/points.js
@@ -26,6 +26,19 @@ export default class Points {
   setFilter(filterType) {
     this._activeFilterType = filterType;
     this._callHandlers(this._filterChangeHandlers);
+
+    removePoint(id) {
+      const pointIndex = this._points.findIndex((point) => point.id === id);
+
+      if (pointIndex === -1) {
+       return false;
+      }
+
+      this._points = [].concat(this._points.slice(0, index), this._points.slice(pointIndex + 1));
+
+      this._callHandlers(this._dataChangeHandlers);
+
+      return true;
   }
 
   updatePoint(id, point) {
@@ -37,7 +50,14 @@ export default class Points {
 
     this._points = [].concat(this._points.slice(0, pointIndex), point, this._points.slice(pointIndex + 1));
 
+    this._callHandlers(this._dataChangeHandlers);
+
     return true;
+  }
+
+  addTask(point) {
+    this.points = [].concat(point, this._points);
+    this._callHandlers(this._dataChangeHandlers);
   }
 
   setFilterChangeHandler(handler) {
